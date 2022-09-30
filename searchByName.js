999e7e76c099d6d48f2481e03a056daa384f628a
Bandla <rbandla@albany.edu>
2022-09-30 10:03:20 -0400

    contents

---
+++ searchByName.js
@@ -0,0 +1,10 @@
+
+let showresults=false;
+
+const getNames=()=>{
+    let firstname=document.getElementById('floatingFirstName').value;
+    let lastname=document.getElementById('flaotingLastName').value;
+    console.log("clicked");
+    console.log(firstname,lastname);
+    showresults=true;
+}
