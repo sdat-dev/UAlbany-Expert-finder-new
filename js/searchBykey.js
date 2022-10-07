ceb01210043ec7c1ff4fdfc81253e60a2b5ab148
RamkishoreBandla <bandlaramkishore090@gmail.com>
2022-10-07 15:45:35 +0530

    search by text

--- js/searchBykey.js
+++ js/searchBykey.js
@@ -7,39 +7,79 @@ function checkFacultiesBasedOnKeywords(keyWord) {
     Object.entries(facultyAbstracts).reduce((matchedFaculties, [key, abstract]) => {
         if (abstract.toLowerCase().includes(keyWord.toLowerCase())) {
             const facultyName = key.split(' ');
-            matchedFaculties.push({ firstName: facultyName[0]||"", lastName: facultyName[1]||"" });
+            matchedFaculties.push({ firstName: facultyName[0] || "", lastName: facultyName[1] || "" });
         }
         return matchedFaculties;
     }, matchedFaculties);
     return matchedFaculties;
 }
 
-const capitalize = (word) =>{ 
-    if(word!=""){
-        let [first,...rest]=word;
-    return first.toUpperCase() + rest.join('').toLowerCase()
+
+
+const capitalize = (word) => {
+    if (word != "") {
+        let [first, ...rest] = word;
+        return first.toUpperCase() + rest.join('').toLowerCase();
     }
-    else{
+    else {
         return "";
     }
 }
 
-document.getElementById('get-results').addEventListener('click',()=>{
-    const keyWord=document.getElementById('input-keyword').value;
-    console.log(keyWord);
-    console.log(checkFacultiesBasedOnKeywords(keyWord));
-    if(keyWord){
-    let facultyNames=checkFacultiesBasedOnKeywords(keyWord);
-    if(facultyNames.length>0){
-        // document.getElementById("collapseOne").c=true;
-        document.getElementsByClassName("accordion-body")[0].innerHTML=``;
-        let facultyHtmlContent=facultyNames.map((faculty,i)=>`<tr>
+const getKeyWordsFromText = (text) => {
+
+    let list_of_keywords = [];
+    for (let [k, v] of Object.entries(tree3)) {
+        if (text.toLowerCase().includes(k.toLowerCase())) {
+            // console.log(k);
+            list_of_keywords.push(k);
+        }
+    }
+
+    
+
+    return list_of_keywords;
+}
+
+
+const sortByScore=(keywords)=>{
+let facObj={};
+    for(let keyword of keywords){
+        let faNames=checkFacultiesBasedOnKeywords(keyword);
+        for(let i of faNames){
+            // console.log(i);
+            let name=i.firstName+" "+i.lastName;
+            if(facObj[name]){
+                facObj[name]+=1;
+            }
+            else{
+            facObj[name]=1;
+            }
+        }
+    }
+    console.log(facObj);
+}
+
+document.getElementById('get-results').addEventListener('click', () => {
+    const keyWord = document.getElementById('input-keyword').value;
+
+    if (keyWord) {
+        let matchedWords = getKeyWordsFromText(keyWord);
+        console.log(matchedWords);
+
+        let facultyNames = checkFacultiesBasedOnKeywords(keyWord);
+        let sortedFaculty=sortByScore(matchedWords);
+
+        if (facultyNames.length > 0) {
+            // document.getElementById("collapseOne").c=true;
+            document.getElementsByClassName("accordion-body")[0].innerHTML = ``;
+            let facultyHtmlContent = facultyNames.map((faculty, i) => `<tr>
          <th scope="row">${i}</th>
          <td>${capitalize(faculty.firstName)}</td>
          <td>${capitalize(faculty.lastName)}</td>
          </tr>`);
-        
-        document.getElementById("results-accordian").innerHTML=`<table class="table table-hover">
+
+            document.getElementById("results-accordian").innerHTML = `<table class="table table-hover">
         <thead>
         <tr>
           <th scope="col">#</th>
@@ -47,11 +87,11 @@ document.getElementById('get-results').addEventListener('click',()=>{
           <th scope="col">Last name</th>
         </tr>
       </thead>
-      <tbody>`+ facultyHtmlContent.join("") +`</tbody></table>`;
+      <tbody>`+ facultyHtmlContent.join("") + `</tbody></table>`;
+        }
     }
-}
-    else{
+    else {
         //no faculty found
-        document.getElementById("results-accordian").innerHTML=`<p>There are no matches for the given input</p>`;
+        document.getElementById("results-accordian").innerHTML = `<p>There are no matches for the given input</p>`;
     }
 })
