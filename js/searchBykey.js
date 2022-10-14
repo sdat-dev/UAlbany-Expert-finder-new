7f038867f80b7e71da127b56363e23c8dd104f37
Bandla <rbandla@albany.edu>
2022-10-14 17:18:56 -0400

    updated text scoring logic for search by text

--- js/searchBykey.js
+++ js/searchBykey.js
@@ -1,7 +1,8 @@
 import tree3 from '../JSONs/tree3.json' assert {type: 'json'};;
 import facultyAbstracts from '../JSONs/PI_Abstract.json' assert {type: 'json'};
-import keyFacMapper from '../JSONs/keyFacMapper.json' assert {type: 'json'};
 import profNameTFIDFscore from '../JSONs/ProfnameTFIDFscore.json' assert {type: 'json'};
+import keyFacMapper from '../JSONs/keyFacMapper.json' assert {type: 'json'};
+
 // import {TfIdf} from '../js/TFIDF.js';
 // const natural = require('natural')
 //  const TfIdf = natural.TfIdf;
